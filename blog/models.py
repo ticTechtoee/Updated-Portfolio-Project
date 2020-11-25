d795ffb57f6b6c2d5af151ec545c8e5e6b01bf2a
Farhan Basheer <farhanfb6@outlook.com>
2020-11-25 13:45:33 +0500

    Updated the Blog

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField(max_length = 100)
-    pub_date = models.CharField(max_length = 12)
-    body = models.CharField(max_length = 400)
+    title = models.CharField(max_length = 255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
-    
     
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-        'job.apps.JobConfig',
+     'job.apps.JobConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
