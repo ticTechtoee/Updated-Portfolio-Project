5b148785bbe3ac0b9ef0abea0234b747a993d432
Farhan Basheer <farhanfb6@outlook.com>
2020-11-25 13:14:35 +0500

    Updated the jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 100)
+    pub_date = models.CharField(max_length = 12)
+    body = models.CharField(max_length = 400)
+    image = models.ImageField(upload_to='images/')
+    
+    
