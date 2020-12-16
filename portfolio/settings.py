d4e6a05aaceeecb59cce2826f0e75df25872f702
Farhan Basheer <farhanfb6@outlook.com>
2020-12-16 17:18:56 +0500

    Blog Functionality and Polish And Fixed the Name of Resume again

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'farhan60',
+        'PASSWORD':'check the pass file in django folder',
         'HOST':'localhost',
         'PORT':'5432',
     }
